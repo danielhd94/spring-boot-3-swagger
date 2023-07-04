7cafde1b1a93ebb11ada8477f00337ae2ead2843
Daniel Hernandez <danielhd94@hotmail.com>
2023-07-04 15:42:20 -0700

    first commit

---
+++ src/main/java/com/springboot/app/swagger/SpringbootItemService1Application.java
@@ -0,0 +1,13 @@
+package com.springboot.app.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootItemService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootItemService1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/app/swagger/SpringbootItemService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.app.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootItemService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
